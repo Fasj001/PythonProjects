a67d6781ffb29ed576f9727bfd74661966de12f7
Fasj001 <fasj001@student.kristiania.no>
2024-04-21 17:06:10 +0200

    Just trying to make a password manager

--- Projects/Project 2/Project 2.py
+++ Projects/Project 2/Project 2.py
@@ -1,1 +1,27 @@
-print("Project 2")
+import tkinter as tk
+from tkinter import messagebox  
+
+
+def add():
+    username = entryName.get()
+    password = entryPassword.get()
+    if username and password:
+        with open('password.txt', 'a') as f:
+            f.write(f"{username} {password}\n"):
+        messagebox.showinfo("Success", "Password added!")
+    else:
+        messagebox.showerror("Error", "Please enter both the fields"):
+
+def get():
+    
+    username = entryName.get()
+    
+    passwords={}
+    try:
+        with open("passwords.txt", 'r') as f:
+            for k in f:
+                i = k.split(' ')
+                # creating the key-value pair of username and password.
+                passwords[i[0]] = i[1]
+    except:
+        print("Error")
